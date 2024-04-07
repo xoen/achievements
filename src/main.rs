97a072553ac4b7e9315b03732e1efc82a1bfb30d
Aldo "xoen" Giambelluca <aldo.giambelluca@gmail.com>
2024-04-07 18:14:40 +0100

    Added clap and scheleton of subcommands
    At the moment adding/removing days from the config
    is not implemented.

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,38 @@
+use clap::{command, Parser, Subcommand};
+
 use achievements::{
     config::{self, Day},
     days_since, Interval,
 };
 
+#[derive(Subcommand)]
+enum Command {
+    /// Displays the achivements
+    Achievements,
+    /// Adds a day to the config
+    Add,
+    /// Removes a day from the config
+    Remove,
+}
+
+#[derive(Parser)]
+#[command(author, version, about, long_about = None)]
+struct Cli {
+    #[command(subcommand)]
+    command: Option<Command>,
+}
+
 fn main() {
+    let cli = Cli::parse();
+    match cli.command.unwrap_or(Command::Achievements) {
+        Command::Achievements => {
+            display_achievements();
+        }
+        _ => unimplemented!("Other commands not implemented yet"),
+    }
+}
+
+fn display_achievements() {
     let config = config::read().expect("Failed to read config");
 
     for Day { label, date: day } in config.days.iter() {
